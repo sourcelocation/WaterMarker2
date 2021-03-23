1c8228ba2e79fd76bf71fc45dc6e9dbbdb16906e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-23 20:41:51 +0300

    Initial Commit

---
+++ WaterMarker2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WaterMarker2
+//
+//  Created by Матвей Анисович on 3/23/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
